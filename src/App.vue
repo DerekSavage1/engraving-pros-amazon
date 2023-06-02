c743b2b8d6f4db39f60e3109fd8e21b047ffc71e
DerekSavage1 <57323958+DerekSavage1@users.noreply.github.com>
2023-06-02 00:48:15 -0700

    sketching new component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div id="app">
+    <Homepage_Footer class="fixed-bot" />
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import HelloWorld from "./components/HelloWorld.vue";
+import Homepage_Footer from "./components/Homepage_Footer.vue";
 
 @Options({
   components: {
     HelloWorld,
+    Homepage_Footer,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style>
+body,
+html {
+  padding: 0;
+  margin: 0;
+}
 #app {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.fixed-bot {
+  margin-top: auto;
 }
 </style>
---
+++ src/components/Homepage_Footer.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="container">
+    <div id="logo">Engraving Pros</div>
+    <div class="navbar">
+      <div class="nav">hi</div>
+      <div class="nav">hello</div>
+      <div class="nav">hola</div>
+    </div>
+    <div class="navbar">
+      <div class="nav">hello</div>
+      <div class="nav">hola</div>
+    </div>
+    <div class="line-segment"></div>
+    <div class="copyright">
+      © 2022 Engraving Pros. <br />
+      All rights reserved.
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#container {
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: auto;
+  background-color: grey;
+  border-top: 5pt solid yellow;
+  padding-left: 64px;
+  padding-right: 64px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  box-sizing: border-box;
+  gap: 10px;
+}
+
+#logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: aqua;
+  height: 46px;
+  width: auto;
+}
+
+.navbar {
+  background-color: antiquewhite;
+  display: flex;
+  flex-direction: row;
+  gap: 48px;
+  align-items: center;
+  justify-content: center;
+}
+.nav {
+  background-color: aquamarine;
+}
+
+.line-segment {
+  height: 3pt;
+  background-color: black;
+  width: auto;
+}
+
+.copyright {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+</style>
