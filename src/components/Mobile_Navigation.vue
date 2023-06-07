2658e2f9e304a2b6a449cd4a27299aac93dae5be
DerekSavage1 <57323958+DerekSavage1@users.noreply.github.com>
2023-06-07 10:49:22 -0700

    Begining Navigation Component

---
+++ src/components/Mobile_Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="container">
+    <div id="logo-hamburger-group">hi</div>
+  </div>
+</template>
+
+<style scoped>
+#container {
+  background-color: blanchedalmond;
+  height: 128px;
+  width: auto;
+  padding: 15px;
+}
+
+#logo-hamburger-group {
+  background-color: darkgoldenrod;
+  justify-content: space-between;
+  width: 100%;
+}
+</style>
