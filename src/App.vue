368518ef4e3c78ff6fa81c536ec6aecaf5cd1080
DerekSavage1 <57323958+DerekSavage1@users.noreply.github.com>
2023-06-07 18:37:33 -0700

    Consistant Naming

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <template>
   <div id="app">
-    <Mobile_Navigation class="fixed-top" />
-    <Homepage_Footer class="fixed-bot" />
+    <MobileNavigation class="fixed-top" />
+    <MobileFooter class="fixed-bot" />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
-import Homepage_Footer from "./components/Homepage_Footer.vue";
-import Mobile_Navigation from "./components/Mobile_Navigation.vue";
+import MobileFooter from "./components/MobileFooter.vue";
+import MobileNavigation from "./components/MobileNavigation.vue";
 
 @Options({
   components: {
-    HelloWorld,
-    Homepage_Footer,
-    Mobile_Navigation,
+    MobileFooter,
+    MobileNavigation,
   },
 })
 export default class App extends Vue {}
