c9f4e73bde68731f058cf1ea74e85d6d35289fb6
DerekSavage1 <57323958+DerekSavage1@users.noreply.github.com>
2023-06-08 10:14:03 -0700

    changed to fixed

--- src/components/MobileNavigation.vue
+++ src/components/MobileNavigation.vue
@@ -19,15 +19,23 @@
   box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.3),
     0px 1px 3px 1px rgba(0, 0, 0, 0.15);
   display: flex;
+  flex-direction: row;
+  position: fixed;
   justify-content: center;
   align-items: center;
-  width: 100vw;
-  height: 100vh;
-  background-color: white;
+  top: 0;
+  left: 0;
+  width: 100%;
   height: 128px;
-  width: auto;
+  background-color: white;
   padding-left: 15px;
   padding-right: 15px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  box-sizing: border-box;
+  gap: 10px;
+  color: white;
+  z-index: 9999;
 }
 
 #hamburger {
